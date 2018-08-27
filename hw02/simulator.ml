61fcd218425ed8a5762908c30259f1658dec6477
Alireza Alidousti <alireza.eva.u23@gmail.com>
2018-08-27 15:26:34 -0700

    refactors to add label_index_map and segment_size helper fun

--- hw02/simulator.ml
+++ hw02/simulator.ml
@@ -572,25 +572,7 @@ exception Redefined_sym of lbl
   HINT: List.fold_left and List.fold_right are your friends.
  *)
 
-let text_segment_size = fun p ->
-  let aux = fun count elm ->
-    match elm.asm with
-    | Text(ins) -> count + (List.length ins) * 8
-    | _ -> count
-  in
-  List.fold_left aux 0 p
-
-let text_label_index_map = fun p ->
-  let aux = fun (map, curr_index) elm ->
-    match elm.asm with
-    | Text(ins) -> (
-      (elm.lbl, curr_index)::map,
-      curr_index + (List.length ins) * 8
-    )
-    | _ -> (map, curr_index)
-  in
-  match (List.fold_left aux ([], 0) p) with
-  | (map, _) -> map
+module SS = Set.Make(String)
 
 let rec data_size data_list count =
   match data_list with
@@ -598,27 +580,28 @@ let rec data_size data_list count =
   | (Quad(q))::tail -> data_size tail (count + 8)
   | _ -> count
 
-let data_segment_size p =
+let segment_size = fun p ->
   let aux = fun count elm ->
     match elm.asm with
+    | Text(ins) -> count + (List.length ins) * 8
     | Data(data) -> data_size data 0
-    | _ -> count
   in
   List.fold_left aux 0 p
 
-let data_label_index_map = fun p ->
+let label_index_map = fun p ->
   let aux = fun (map, curr_index) elm ->
     match elm.asm with
+    | Text(ins) -> (
+      (elm.lbl, curr_index)::map,
+      curr_index + (List.length ins) * 8
+    )
     | Data(data) -> (
       (elm.lbl, curr_index)::map,
       curr_index + (data_size data 0)
     )
-    | _ -> (map, curr_index)
   in
   match (List.fold_left aux ([], 0) p) with
   | (map, _) -> map
-
-module SS = Set.Make(String)
 
 let labels_are_unique p =
   let rec aux = fun p s ->
